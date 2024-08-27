f4e6fcdc093676ffc9b4d8fb6b0abc3ebc03fd7c
Larry Xue <larry.p.xue@gmail.com>
2024-08-27 02:29:49 -0400

    remove filename check

--- src/texteditor.cpp
+++ src/texteditor.cpp
@@ -12,11 +12,6 @@ int main(int argc, const char** argv) {
 		return 1;
 	}
 	std::string filename = argv[1];
-	if (filename.find_first_of("*?|\"") != std::string::npos || filename[0] == '-' ||
-		std::find_if(filename.begin(), filename.end(), iscntrl) != filename.end()) {
-		std::cerr << "Invalid filename" << std::endl;
-		return 1;
-	}
 	std::ios_base::sync_with_stdio(false);
 	std::vector<std::string> args{argv + 2, argv + argc};
 	Editor editor{filename, args};
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,4 +1,5 @@
 #ifndef UTILS_H
+#define UTILS_H
 #include <string>
 std::string replace_all(std::string str, const std::string& search, const std::string& replace);
 #endif
